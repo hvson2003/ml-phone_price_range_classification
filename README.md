f15bf44ce197f7f6a8afdacc16fac3a9d6317d88
hvson03 <hvson2003@gmail.com>
2024-05-19 11:45:21 +0700

    add comparative parameter Precision, Recall, F1-core and update note README

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Dự án bao gồm:
-<div>Xây dựng giao diện dự đoán phân giá điện thoại qua các thông số của nó</div>
-<div>Sử dụng 3 mô hình phân lớp chính: Logistic Regression, Perceptron, Neural Network vá đánh giá so sánh 3 mô hình</div>
-<div>Sử dùng mô hình học kết hợp: Stacking và đánh giá với 3 mô hình trên</div>
-<div>(Dữ liệu đã được gán nhãn sẵn lấy từ trang web Kaggle)</div>
+<div><b>Bài toán Xây dựng giao diện dự đoán phân giá điện thoại qua các thông số của nó</b>></div>
+<div>Thực hiện train riêng từng model phân lớp Logistic Regression, Perceptron, Neural Network và tìm các tham số phù hợp nhất</div>
+<div>Sử dùng mô hình học kết hợp Stacking và đánh giá với 3 mô hình trên</div>
+<div><b>Kết quả chạy so sánh 4 model:</b></div>
+<img src="img/compare_models.png"/>
+<br>
+<div><b>Kết quả chạy GUI (GUI sử dung LogisticRegression với kết quả tốt nhất ):</b></div>
+<img src="img/GUI.png"/>
+<div></div><i>(Dữ liệu đã được gán nhãn sẵn lấy từ trang web Kaggle)</i>
--- compare_models.py
+++ compare_models.py
@@ -1,15 +1,12 @@
 import pandas as pd
 from sklearn.ensemble import StackingClassifier
 from sklearn.linear_model import LogisticRegression, Perceptron
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
-# Đọc dữ liệu
 phone_data = pd.read_csv('phoneprice_dataset.csv')
-
-# Chia tập dữ liệu thành tập huấn luyện và tập kiểm thử
 dt_Train, dt_Test = train_test_split(phone_data, test_size=0.3, shuffle=True)
 
 X_train = dt_Train.iloc[:, :20]
@@ -17,7 +14,6 @@ y_train = dt_Train.iloc[:, 20]
 X_test = dt_Test.iloc[:, :20]
 y_test = dt_Test.iloc[:, 20]
 
-# Tiêu chuẩn hóa dữ liệu
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
@@ -26,20 +22,17 @@ X_test_scaled = scaler.transform(X_test)
 perceptron_model = Perceptron(penalty='l1', alpha=0.001, max_iter=3000, class_weight='balanced')
 perceptron_model.fit(X_train_scaled, y_train.values.ravel())
 perceptron_preds = perceptron_model.predict(X_test_scaled)
-print("Perceptron Accuracy:", accuracy_score(y_test, perceptron_preds))
 
 # Mô hình Logistic Regression
 logistic_model = LogisticRegression(penalty='l2', solver='lbfgs', C=10)
 logistic_model.fit(X_train_scaled, y_train.values.ravel())
 logistic_preds = logistic_model.predict(X_test_scaled)
-print("Logistic Regression Accuracy:", accuracy_score(y_test, logistic_preds))
 
 # Mô hình Neural Network (MLPClassifier)
 grid_search_nn = MLPClassifier(solver='lbfgs', max_iter=3000, alpha=0.001)
 grid_search_nn.fit(X_train_scaled, y_train)
 best_nn_model = grid_search_nn
 nn_preds = best_nn_model.predict(X_test_scaled)
-print("Neural Network Accuracy:", accuracy_score(y_test, nn_preds))
 
 # Mô hình học kết hợp (Stacking Classifier)
 base_models = [('perceptron', perceptron_model), ('logistic', logistic_model), ('neural_network', best_nn_model)]
@@ -47,4 +40,32 @@ final_estimator = LogisticRegression(penalty='l2', solver='lbfgs', C=10)
 stacking_model = StackingClassifier(estimators=base_models, final_estimator=final_estimator)
 stacking_model.fit(X_train_scaled, y_train.values.ravel())
 stacking_preds = stacking_model.predict(X_test_scaled)
-print("Stacking Classifier Accuracy:", accuracy_score(y_test, stacking_preds))
+
+models = ["Perceptron", "Logistic Regression", "Neural Network", "Stacking Classifier"]
+accuracies = [accuracy_score(y_test, perceptron_preds),
+              accuracy_score(y_test, logistic_preds),
+              accuracy_score(y_test, nn_preds),
+              accuracy_score(y_test, stacking_preds)]
+precisions = [precision_score(y_test, perceptron_preds, average='weighted'),
+              precision_score(y_test, logistic_preds, average='weighted'),
+              precision_score(y_test, nn_preds, average='weighted'),
+              precision_score(y_test, stacking_preds, average='weighted')]
+recalls = [recall_score(y_test, perceptron_preds, average='weighted'),
+           recall_score(y_test, logistic_preds, average='weighted'),
+           recall_score(y_test, nn_preds, average='weighted'),
+           recall_score(y_test, stacking_preds, average='weighted')]
+f1_scores = [f1_score(y_test, perceptron_preds, average='weighted'),
+             f1_score(y_test, logistic_preds, average='weighted'),
+             f1_score(y_test, nn_preds, average='weighted'),
+             f1_score(y_test, stacking_preds, average='weighted')]
+
+# Create a DataFrame to store and display the metrics
+metrics_df = pd.DataFrame({
+    "Model": models,
+    "Accuracy": accuracies,
+    "Precision": precisions,
+    "Recall": recalls,
+    "F1-Score": f1_scores
+})
+
+print(metrics_df)
